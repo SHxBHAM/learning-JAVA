38484946b47821ce057bc58be1844004c537df7c
SHxBHAM <shubhammishra41880@gmail.com>
2024-01-14 12:34:53 +0530

    about primitive data types

---
+++ primitives.java
@@ -0,0 +1,15 @@
+public class primitives {
+    public static void main(String[] args) {
+        // primitive data types are those data types which cannot be further broken down or divided into smaller units.
+        int integer = 7;
+        char character = 'a'; // character are declared using ' ' single quotes.
+        float floatingPoint = 2891.21f; //floating point numbers are stored in float type data types we need to end the declaration with 'f' or 'F'
+        double largefloat = 324787.123784392; //double is used for large decimal numbers
+        long largeint = 3298479764732l;//long integers are stored in long data type we need to end the declaration with 'l' or 'L'
+        // String is not a primitive datatype as it can be further broken down in char type as individual characters
+        String name = "SHUBHXM"; //strings are declared using " " double quotes.
+        //we use 'f' in end of declaration in float types because default its set to take all the decimals as double
+        //Similarly we use 'l' in declaration of long typed because default its set to take all numbers as int.
+        
+    }
+}
